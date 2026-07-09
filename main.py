d233dee015e6c4f03796f7b5e0445411a6967206
PCarlosJr01 <pcarlosjr01@gmail.com>
2026-07-08 18:56:25 -0700

    feat: add selection list

--- main.py
+++ main.py
@@ -6,8 +6,16 @@ def main():
     print("Windows Security Audit Tool")
     print("=" * 50)
     print()
+    print("Enter a valid number 1-10")
+    print("=" * 50)
+    print("1. Firewall Audit")
+    print()
 
-    run_firewall_audit()
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        run_firewall_audit()
+    else:
+        print("Invalid choice. Please enter a number between 1 and 10.")
 
 
 if __name__ == "__main__":
